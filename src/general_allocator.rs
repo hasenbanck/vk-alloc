36b4be03a0c21d1fe14e5820b2bdabc41954d2c9
Nils Hasenbanck <nils@hasenbanck.de>
2021-02-14 13:41:30 +0100

    First iteration of the allocation of the general allocator.

--- src/general_allocator.rs
+++ src/general_allocator.rs
@@ -1,13 +1,12 @@
 //! Implements the general allocator.
 
-use std::collections::HashMap;
 use std::ffi::c_void;
-use std::num::NonZeroU64;
 
 use ash::version::InstanceV1_0;
 use ash::vk;
+use slotmap::{new_key_type, SlotMap};
 
-use crate::Result;
+use crate::{align_up, Result};
 use crate::{debug_memory_types, Allocation, MemoryBlock};
 
 /// The general purpose memory allocator. Implemented as a free list allocator.
@@ -18,9 +17,7 @@ pub struct GeneralAllocator {
     memory_types: Vec<MemoryType>,
     buffer_pools: Vec<MemoryPool>,
     image_pools: Vec<MemoryPool>,
-    logical_device: ash::Device,
     memory_properties: vk::PhysicalDeviceMemoryProperties,
-    block_size: usize,
 }
 
 impl GeneralAllocator {
@@ -53,25 +50,39 @@ impl GeneralAllocator {
                     .contains(vk::MemoryPropertyFlags::HOST_VISIBLE),
             })
             .collect();
+        let memory_types_size = memory_types.len() as u32;
 
-        let linear_pools = memory_types
+        let buffer_pools = memory_types
             .iter()
-            .map(|_x| MemoryPool::default())
-            .collect();
-        let optimal_pools = memory_types
-            .iter()
-            .map(|_x| MemoryPool::default())
+            .enumerate()
+            .map(|(i, x)| {
+                MemoryPool::new(
+                    i as u32,
+                    (2u64).pow(descriptor.block_size as u32),
+                    x.memory_type_index,
+                    x.is_mappable,
+                )
+            })
             .collect();
 
-        let block_size = (2usize).pow(descriptor.block_size as u32);
+        let image_pools = memory_types
+            .iter()
+            .enumerate()
+            .map(|(i, x)| {
+                MemoryPool::new(
+                    memory_types_size + i as u32,
+                    (2u64).pow(descriptor.block_size as u32),
+                    x.memory_type_index,
+                    x.is_mappable,
+                )
+            })
+            .collect();
 
         Self {
             memory_types,
-            buffer_pools: linear_pools,
-            image_pools: optimal_pools,
-            logical_device: logical_device.clone(),
+            buffer_pools,
+            image_pools,
             memory_properties,
-            block_size,
         }
     }
 
@@ -80,18 +91,40 @@ impl GeneralAllocator {
 
     /// Allocates memory.
     /// TODO
+    ///
+    ///
+    /// For each memory type we have two memory pools: For linear and for optimal textures.
+    /// This removes the need to check for the granularity between them (1k on my 2080s) and
+    /// the idea is, that buffers/textures have different lifetimes and internal fragmentation
+    /// is smaller this way.
+    ///
+    /// Dedicated blocks still exists in their respective pools. They are de-allocated when
+    /// they are freed. Normal blocks are not de-allocated.
+    ///
+    /// Each pool has fixed sized blocks that need to be of power two size. Each block has at
+    /// least one chunk.
+    ///
+    /// Free chunks are saved in a segregated list of power2 sizes. The smallest size is 256 bytes.
+    /// 256 B, 512 B, 1 KiB, 2 B, 4 KiB, 8 KiB, 16 KiB, 32 KiB, 64 KiB, 128 KiB, 256 KiB, 512 KiB,
+    /// 1 MiB, 2 MiB, 4 MiB, 8 MiB, 16 MiB, 32 MiB, 64 MiB, 128 MiB, 512 MiB etc.
+    ///
+    /// The biggest bucket size is the block size.
     #[cfg_attr(feature = "profiling", profiling::function)]
-    pub fn allocate(&self) -> Result<GeneralAllocation> {
+    pub fn allocate(&self, size: u64) -> Result<GeneralAllocation> {
+        // Get the bucket index if the smallest bucket is 256 B of a pow2 bucket list.
+        // bucket 0 =   0 B -  256 B
+        // bucket 1 = 257 B -  512 B
+        // bucket 2 = 513 B - 1024 B
+
+        // TODO test if allocation needs to be dedicated and select the correct memory pool.
+
         Ok(GeneralAllocation {
-            allocator_index: 0,
             pool_index: 0,
-            block_index: 0,
-            chunk_index: 0,
+            chunk_key: Default::default(),
             device_memory: Default::default(),
             offset: 0,
             size: 0,
             mapped_ptr: None,
-            name: None,
         })
     }
 
@@ -120,15 +153,12 @@ impl Default for GeneralAllocatorDescriptor {
 /// An allocation of the `GeneralAllocator`.
 #[derive(Clone, Debug)]
 pub struct GeneralAllocation {
-    allocator_index: u8,
-    pool_index: u8,
-    block_index: u64,
-    chunk_index: u64,
+    pool_index: u32,
+    chunk_key: ChunkKey,
     device_memory: vk::DeviceMemory,
     offset: u64,
     size: u64,
     mapped_ptr: Option<std::ptr::NonNull<c_void>>,
-    name: Option<String>,
 }
 
 impl Allocation for GeneralAllocation {
@@ -154,7 +184,6 @@ impl Allocation for GeneralAllocation {
 }
 
 /// Memory of a specific memory type.
-#[derive(Debug)]
 struct MemoryType {
     memory_properties: vk::MemoryPropertyFlags,
     memory_type_index: usize,
@@ -162,20 +191,209 @@ struct MemoryType {
     is_mappable: bool,
 }
 
+new_key_type! {
+    struct BlockKey;
+    struct ChunkKey;
+}
+
+struct BestFitCandidate {
+    key: ChunkKey,
+    free_list_index: usize,
+    free_size: u64,
+    aligned_size: u64,
+}
+
 /// A managed memory region of a specific memory type.
 /// Used to separate buffer (linear) and texture (optimal) memory regions,
 /// so that internal memory fragmentation is kept low.
-#[derive(Default)]
 struct MemoryPool {
-    blocks: Vec<MemoryBlock>,
-    chunks: HashMap<NonZeroU64, MemoryChunk>,
-    free_chunks: Vec<MemoryChunk>,
+    pool_index: u32,
+    block_size: u64,
+    memory_type_index: usize,
+    is_mappable: bool,
+    blocks: SlotMap<BlockKey, MemoryBlock>,
+    // TODO benchmark the available slotmaps
+    chunks: SlotMap<ChunkKey, MemoryChunk>,
+    // TODO benchmark the available slotmaps
+    free_chunks: Vec<Vec<ChunkKey>>,
+    // TODO this compares to a linked list / slotmap? or Vec<Vec<Option<ChunkKey>>>
+    max_bucket_index: usize,
 }
 
 impl MemoryPool {
-    pub(crate) fn allocate(&mut self) {}
+    fn new(pool_index: u32, block_size: u64, memory_type_index: usize, is_mappable: bool) -> Self {
+        let blocks = SlotMap::with_capacity_and_key(1024);
+        let chunks = SlotMap::with_capacity_and_key(1024);
+
+        // The smallest bucket size is 256b, which is log2(256) = 8. So the maximal bucket size is
+        // "64 - log2(block_size - 1) - 8". We can't have a free chunk that is bigger than a block.
+        let num_buckets = 56usize - (block_size - 1u64).leading_zeros() as usize;
+
+        let empty_list: Vec<ChunkKey> = Vec::with_capacity(1024);
+        let free_chunks = vec![empty_list; num_buckets];
+
+        Self {
+            pool_index,
+            block_size,
+            memory_type_index,
+            is_mappable,
+            blocks,
+            chunks,
+            free_chunks,
+            max_bucket_index: num_buckets,
+        }
+    }
+
+    fn allocate(
+        &mut self,
+        device: &ash::Device,
+        size: u64,
+        alignment: u64,
+    ) -> Result<GeneralAllocation> {
+        let mut bucket_index = calculate_bucket_index(size);
+
+        // Make sure that we don't try to allocate a chunk bigger than the block.
+        debug_assert!(bucket_index <= self.max_bucket_index);
+
+        loop {
+            // We couldn't find an empty block, so we will allocate a new one.
+            if bucket_index > self.max_bucket_index {
+                self.allocate_new_block(device)?
+            }
+
+            let free_list = &self.free_chunks[bucket_index];
+
+            // Find best fit in this bucket.
+            let mut best_fit_candidate: Option<BestFitCandidate> = None;
+            for (index, key) in free_list.iter().enumerate() {
+                let chunk = &mut self.chunks[*key];
+                debug_assert!(chunk.is_free);
+
+                if chunk.size < size {
+                    continue;
+                }
+
+                let offset = align_up(chunk.offset, alignment);
+                let padding = offset - chunk.offset;
+                let aligned_size = padding + size;
+
+                // Try to find the best fitting chunk.
+                if chunk.size >= aligned_size {
+                    let free_size = chunk.size - aligned_size;
+
+                    let best_fit_size = if let Some(best_fit) = &best_fit_candidate {
+                        best_fit.free_size
+                    } else {
+                        u64::MAX
+                    };
+
+                    if free_size < best_fit_size {
+                        best_fit_candidate = Some(BestFitCandidate {
+                            key: *key,
+                            free_list_index: index,
+                            free_size,
+                            aligned_size,
+                        })
+                    }
+                }
+            }
+
+            // Allocate using the best fit candidate.
+            if let Some(candidate) = &best_fit_candidate {
+                self.free_chunks[bucket_index].remove(candidate.free_list_index);
+
+                // Split the lhs chunk and register the rhs as a new free chunk.
+                let rhs_chunk_key = if candidate.free_size != 0 {
+                    let lhs_chunk = self.chunks[candidate.key].clone();
+
+                    let rhs_offset = lhs_chunk.offset + candidate.aligned_size;
+                    let rhs_size = lhs_chunk.size - candidate.aligned_size;
+
+                    let rhx_chunk_key = self.chunks.insert(MemoryChunk {
+                        block_key: lhs_chunk.block_key,
+                        size: rhs_size,
+                        offset: rhs_offset,
+                        previous: Some(candidate.key),
+                        next: lhs_chunk.next,
+                        is_free: false,
+                    });
+
+                    let rhs_bucket_index = calculate_bucket_index(rhs_size);
+                    self.free_chunks[rhs_bucket_index].push(rhx_chunk_key);
+
+                    Some(rhx_chunk_key)
+                } else {
+                    None
+                };
+
+                let chunk = &mut self.chunks[candidate.key];
+                chunk.is_free = false;
+                chunk.size = candidate.aligned_size;
+
+                if let Some(new_chunk_key) = rhs_chunk_key {
+                    chunk.next = Some(new_chunk_key);
+                }
+
+                let block = &self.blocks[chunk.block_key];
+
+                return Ok(GeneralAllocation {
+                    pool_index: self.pool_index,
+                    chunk_key: candidate.key,
+                    device_memory: block.device_memory,
+                    offset: chunk.offset,
+                    size: chunk.size,
+                    mapped_ptr: std::ptr::NonNull::new(block.mapped_ptr),
+                });
+            }
+
+            bucket_index += 1;
+        }
+    }
+
+    fn allocate_new_block(&mut self, device: &ash::Device) -> Result<()> {
+        let block = MemoryBlock::new(
+            device,
+            self.block_size,
+            self.memory_type_index,
+            self.is_mappable,
+        )?;
+        let block_key = self.blocks.insert(block);
+        let chunk_key = self.chunks.insert(MemoryChunk {
+            block_key,
+            size: self.block_size,
+            offset: 0,
+            previous: None,
+            next: None,
+            is_free: true,
+        });
+
+        self.free_chunks[self.max_bucket_index].push(chunk_key);
+
+        Ok(())
+    }
+
+    fn free(&mut self, device: &ash::Device, chunk_key: ChunkKey) -> Result<()> {
+        todo!();
+        Ok(())
+    }
 }
 
-/// A chunk inside a memory block.
-#[derive(Debug)]
-struct MemoryChunk {}
+/// A chunk inside a memory block. Next = None is the start chunk. Previous = None is the end chunk.
+#[derive(Clone)]
+struct MemoryChunk {
+    block_key: BlockKey,
+    size: u64,
+    offset: u64,
+    previous: Option<ChunkKey>,
+    next: Option<ChunkKey>,
+    is_free: bool,
+}
+
+#[inline]
+fn calculate_bucket_index(size: u64) -> usize {
+    if size <= 256 {
+        0
+    } else {
+        56usize - (size - 1u64).leading_zeros() as usize
+    }
+}
